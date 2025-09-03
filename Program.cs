3bf397903b6484aaecb8d82b19ff16962e13b17f
trisha2027 <trisha.vaidya22@pccoepune.org>
2025-09-03 00:18:48 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+        options.SlidingExpiration = true;
+        options.LoginPath = "/Account/Login"; // Redirect here if user is not authenticated
+        options.AccessDeniedPath = "/Home/AccessDenied"; // Optional: page for forbidden access
+    });
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Add Authentication and Authorization middleware HERE
+// The order is important!
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
